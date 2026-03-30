e7c1374fa56a332a9434af0b78b23a0c0c0b875f
Sanjeev Sekar <sankumsek@gmail.com>
2026-03-30 21:22:34 +0000

    chore: remove projects command

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,13 +1,1 @@
 // // List of commands that require API calls
-
-import { getProjects } from '../api';
-
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
-};
